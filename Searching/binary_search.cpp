80406b2fcfd8b3021b606900c23423933fc12637
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2022-09-26 17:44:57 +0600

    Create binary_search.cpp

---
+++ Searching/binary_search.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int binary_search(int *arr, int n, int key) {
+    int first = 0;
+    int last = n - 1;
+
+    while (first <= last) {
+        int mid = (first + last) / 2;
+
+        if (*(arr + mid) == key) {
+            return mid;
+        } else if (*(arr + mid) > key) {
+            last = mid - 1;
+        } else {
+            first = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n; cin >> n;
+    int *arr = new int[n];
+    for (int i=0; i<n; i++) {
+        cin >> *(arr + i);
+    }
+    sort(arr,arr+n);
+  
+    int key; cin >> key;
+    cout << binary_search(arr,n,key) << endl;
+  
+    return 0;
+}
