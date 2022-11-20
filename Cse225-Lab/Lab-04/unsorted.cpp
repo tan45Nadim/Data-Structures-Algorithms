ee9d731012e79af30b4ad578c122fae88aeca53c
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2022-11-20 21:40:32 +0600

    Create unsorted.cpp

---
+++ Cse225-Lab/Lab-04/unsorted.cpp
@@ -0,0 +1,73 @@
+#include "unsortedtype.h"
+
+template<class T>
+UnsortedType<T>::UnsortedType() {
+    length = 0;
+    currentPos = -1;
+}
+
+template<class T>
+bool UnsortedType<T>::isEmpty() {
+    return length == 0;
+}
+
+template<class T>
+bool UnsortedType<T>::isFull() {
+    return length == max_items;
+}
+
+template<class T>
+void UnsortedType<T>::makeEmpty() {
+    length = 0;
+}
+
+template<class T>
+void UnsortedType<T>::Insert(T item) {
+    data[length] = item;
+    length++;
+}
+
+template<class T>
+void UnsortedType<T>::Delete(T item) {
+    int pos;
+    for(int i=0; i<length; i++)
+    {
+        if(data[i] == item) {
+            pos = i;
+            break;
+        }
+    }
+    for(int i=pos; i<length-1; i++)
+    {
+        data[i] = data[i+1];
+    }
+    length--;
+}
+
+template<class T>
+int UnsortedType<T>::LengthIs() {
+    return length;
+}
+
+template<class T>
+void UnsortedType<T>::getNextItem(T& item) {
+    currentPos++;
+    item = data[currentPos];
+}
+
+template<class T>
+void UnsortedType<T>::ResetList() {
+    currentPos = -1;
+}
+
+template<class T>
+void UnsortedType<T>::RetriveItem(T& item, bool& found) {
+    for(int i=0; i<length; i++)
+    {
+        if(data[i] == item)
+        {
+            found = true;
+            break;
+        }
+    }
+}
