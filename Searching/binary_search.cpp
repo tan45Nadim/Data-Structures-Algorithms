63665e95bdb6a5fe5188930c30b2447da8da045b
Tanjir Ahmed Nadim <106719777+tan45Nadim@users.noreply.github.com>
2024-02-11 00:43:10 +0600

    Update binary_search.cpp

--- Searching/binary_search.cpp
+++ Searching/binary_search.cpp
@@ -31,6 +31,43 @@ int main() {
     cout << "Enter a number for Search: ";
     int key; cin >> key;
     cout << binarySearch(arr, n, key) << endl;
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int binarySearch(int *arr,int first, int last, int key) {
+    if (last >= first) {
+        int mid = (first + last) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] > key) {
+            return binarySearch(arr, first, mid - 1, key);
+        } else {
+            return binarySearch(arr, mid + 1, last, key);
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n; cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> *(arr + i);
+    }
+    sort(arr, arr+n);
+
+    cout << "Enter a number for Search: ";
+    int key; cin >> key;
+    cout << binarySearch(arr, 0, n - 1, key) << endl;
+
+    delete[] arr;
+    return 0;
+}
+
+// Time Complexity O(log N)
+// Space Complexity of O(log N)
 
     return 0;
 }
